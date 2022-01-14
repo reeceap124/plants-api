7bb9f65b3be8431c218ca8e73a657b04263b48d8
Reece Pierson <piersonreece@gmail.com>
2022-01-13 23:03:28 -0700

    more logs

--- utils/pool.js
+++ utils/pool.js
@@ -1,23 +1,25 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg')
 
-const dotenv = require("dotenv");
+const dotenv = require('dotenv')
 
-dotenv.config();
+dotenv.config()
 const dbString = () => {
-  if (process.env.NODE_ENV === "test") {
-    return process.env.TEST_DATABASE_URL;
+  if (process.env.NODE_ENV === 'test') {
+    console.log('ENV is test')
+    return process.env.TEST_DATABASE_URL
   }
-  return process.env.DATABASE_URL;
-};
-connectionString = dbString();
-const databaseConfig = { connectionString };
-const pool = new Pool(databaseConfig);
-pool.on("connect", (err, client) => {
-  console.log("connected to db");
-});
-pool.on("error", (err, client) => {
-  console.error("Unexpected error on idle client", err);
-  process.exit(-1);
-});
+  return process.env.DATABASE_URL
+}
+const connectionString = dbString()
+console.log('connection string', connectionString)
+const databaseConfig = { connectionString }
+const pool = new Pool(databaseConfig)
+pool.on('connect', (err, client) => {
+  console.log('connected to db')
+})
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+})
 
-module.exports = pool;
+module.exports = pool
